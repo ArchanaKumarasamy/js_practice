a3c73081fe5b07f784b580e82d90f53fd41870fc
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-06 18:47:17 +0530

    Ch3_pgm_01.js completed

--- JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_01.js
+++ JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_01.js
@@ -7,6 +7,24 @@ bookTitle = "The Hobbit";
 bookAuthor = "J. R. R. Tolkien";
 
 console.log(bookTitle + " by " + bookAuthor);
+var bookTitle2 = "The Hobbit";
+var bookAuthor2 = "J. R. R. Tolkien";
+console.log(bookTitle2 + " by " + bookAuthor2);
+var bookTitle3 = "The Hobbit";
+var bookAuthor3 = "J. R. R. Tolkien";
+console.log(bookTitle3 + " by " + bookAuthor3);
+var bookTitle4 = "The Hobbit";
+var bookAuthor4 = "J. R. R. Tolkien";
+console.log(bookTitle4 + " by " + bookAuthor4);
+var bookTitle5 = "The Hobbit";
+var bookAuthor5 = "J. R. R. Tolkien";
+console.log(bookTitle5 + " by " + bookAuthor5);
+var bookTitle6 = "The Hobbit";
+var bookAuthor6 = "J. R. R. Tolkien";
+console.log(bookTitle6 + " by " + bookAuthor6);
+var bookTitle7 = "The Hobbit";
+var bookAuthor7 = "J. R. R. Tolkien";
+console.log(bookTitle7 + " by " + bookAuthor7);
 
 
 
