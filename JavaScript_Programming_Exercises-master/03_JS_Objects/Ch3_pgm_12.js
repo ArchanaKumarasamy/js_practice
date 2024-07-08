d32cafcaaab67546a03216b15d18d139ce598131
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-08 21:50:44 +0530

    completed

--- JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_12.js
+++ JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_12.js
@@ -7,6 +7,22 @@ var post = {
 	created : "2015-06-21",
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
+console.log("post id: " + post.id);
+console.log("title: " + post.title);
+console.log("author: " + post.author);
+
+
+var post2 ={
+	id : 2,
+	title : "dream of the red chamber",
+	author : "ca0 xueqin",
+	created : "1971",
+	body : "family saga"
+
+};
+console.log("post id: " + post2.id);
+console.log("title: " + post2.title);
+console.log("author: " + post2.author);
 
 
 
