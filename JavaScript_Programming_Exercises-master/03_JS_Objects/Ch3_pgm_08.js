ce03c7bfed1506293ab081a6b97e7db35ae56a44
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-08 21:24:24 +0530

    ch3_pgm_08.js completed

--- JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_08.js
+++ JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_08.js
@@ -7,9 +7,17 @@ book = {
 	author    : "J. R. R. Tolkien",
 	published : 1937
 };
+console.log(book.published);
 
-console.log(book.title);
-console.log(book.author);
+book2 = {
+    title     : "Northern Lights",
+    author    : "Philip Pullman",
+    published : 1995
+};
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
+
 
 
 
