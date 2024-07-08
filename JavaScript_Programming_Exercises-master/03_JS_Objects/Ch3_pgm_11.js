1723585b6eb0c43fb5c9a0c34f078480e78b9230
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-08 21:44:40 +0530

    completed

--- JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_11.js
+++ JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_11.js
@@ -1,17 +1,31 @@
 // Using a property in a calculation
 
 var player1;
+var player2;
 
 player1 = {
 	  name: "Max",
 	  score: 0
 };
+player2 ={
+	name: "mac",
+	score:100
+}
 
 console.log(player1.name + " has scored " + player1.score);
 
 player1.score = player1.score + 50;
 
 console.log(player1.name + " has scored " + player1.score);
+player1.score = player1.score * 1.10;
+
+console.log(player1.name + " has scored " + player1.score);
+var totalScore = player1.score + player2.score;
+
+console.log(player1.name + " and " + player2.name + " have a combined score of " + totalScore);
+
+
+
 
 
 
