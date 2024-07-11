dead5bc51158543329cd0a9fed0004e039acce7a
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-11 14:29:05 +0530

    completed

--- JavaScript_Programming_Exercises-master/06_JS_Return_values/Ch6_pgm_05.js
+++ JavaScript_Programming_Exercises-master/06_JS_Return_values/Ch6_pgm_05.js
@@ -2,11 +2,12 @@
 
 var totalCost;
 
-totalCost = function (callOutCharge, costPerHour, numberOfHours) {
-	return callOutCharge + costPerHour * numberOfHours;
+totalCost = function (callOutCharge, costPerHour, numberOfHours,discount) {
+	var total= callOutCharge + costPerHour * numberOfHours;
+	return total-discount;
 };
 
-console.log("$" + totalCost(30, 40, 3));
+console.log("$" + totalCost(30, 40, 3,20));
 
 
 
