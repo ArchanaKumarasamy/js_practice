5a912396b282fcdf3bf6cc7d6d6dae4befc5e420
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-11 19:56:05 +0530

    completed

--- JavaScript_Programming_Exercises-master/07_JS_Object_Arguments/Ch7_pgm_03.js
+++ JavaScript_Programming_Exercises-master/07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -27,6 +27,35 @@ console.log(getPlanetInfo(planet2));
 
 
 
+
+
+var buildCar;
+var getCarInfo;
+var car1;
+var car2;
+
+buildCar = function (make, model, year, color, mileage) {
+    return {
+        make: make,
+        model: model,
+        year: year,
+        color: color,
+        mileage: mileage
+    };
+};
+
+getCarInfo = function (car) {
+    return car.year + " " + car.make + " " + car.model + " (" + car.color + ") with " + car.mileage + " miles";
+};
+
+car1 = buildCar("Toyota", "Corolla", 2015, "Silver", 50000);
+car2 = buildCar("Ford", "Mustang", 2018, "Red", 20000);
+
+console.log(getCarInfo(car1));
+console.log(getCarInfo(car2));
+
+
+
 /* Further Adventures
  *
  * 1) Write a similar program to create and
