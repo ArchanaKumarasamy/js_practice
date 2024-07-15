5ab399a2c240217a664423bad535c10961042fcf
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-15 20:26:38 +0530

    completed

--- JavaScript_Programming_Exercises-master/12_JS_Sets_Maps/Ch12_pgm_02.js
+++ JavaScript_Programming_Exercises-master/12_JS_Sets_Maps/Ch12_pgm_02.js
@@ -2,6 +2,15 @@ const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 const countries = ['Finland', 'Sweden', 'Norway']
 
+const union = new Set([...a, ...b]);
+console.log(union);
+
+const intersection = new Set(a.filter(num => b.includes(num)));
+console.log(intersection); 
+ 
+const difference = new Set(a.filter(num => !b.includes(num)));
+console.log(difference); 
+
 // Find a union b
 // Find a intersection b
 // Find a with b
