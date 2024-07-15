869c0499641657b4a158c69e2342d277cf5894ab
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-15 15:00:12 +0530

    completed

--- JavaScript_Programming_Exercises-master/11_JS_Promises/Ch11_pgm_05.js
+++ JavaScript_Programming_Exercises-master/11_JS_Promises/Ch11_pgm_05.js
@@ -1,5 +1,28 @@
 const countriesAPI = 'https://restcountries.com/v2/all'
-const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+const fetchCountriesData = async () => {
+    try {
+      const response = await fetch(countriesAPI);
+      const countries = await response.json();
+  
+      countries.forEach(country => {
+        const { name, capital, languages, population, area } = country;
+        const languageNames = languages.map(lang => lang.name).join(', ');
+  
+        console.log(`Name: ${name}`);
+        console.log(`Capital: ${capital}`);
+        console.log(`Languages: ${languageNames}`);
+        console.log(`Population: ${population}`);
+        console.log(`Area: ${area}`);
+        console.log('------------------------');
+      });
+    } catch (error) {
+      console.error('Error fetching countries data:', error);
+    }
+  };
+  
+
+  fetchCountriesData();
+  
 
 // Read the countries API using fetch and print the name of country, capital, languages, 
 // population and area.
