cc2be25fc9465651dd3aced4f5d194f05c50f453
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-15 20:21:59 +0530

    completed

--- JavaScript_Programming_Exercises-master/12_JS_Sets_Maps/Ch12_pgm_01.js
+++ JavaScript_Programming_Exercises-master/12_JS_Sets_Maps/Ch12_pgm_01.js
@@ -2,6 +2,26 @@ const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 const countries = ['Finland', 'Sweden', 'Norway']
 
+const emptySet = new setInterval();
+console.log(emptySet);
+
+const numberSet = new Set();
+for (let ii=0; i <=10; i++){
+    numberSet.add(i);
+}
+console.log(numberSet);
+
+numberSet.delete(5);
+console.log(numberSet);
+const stringArray = ['apple','banana','cherry','date','elderberry']
+const stringSet = new Set(stringArray);
+console.log(stringSet); 
+
+const countryMap = new Map();
+countries.forEach(country => {
+  countryMap.set(country, country.length);
+});
+console.log(countryMap);
 
 // create an empty set
 // Create a set containing 0 to 10 using loop
