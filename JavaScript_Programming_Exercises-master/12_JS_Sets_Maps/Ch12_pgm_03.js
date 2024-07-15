ad0c13005f8933a4d410aa724c01919c9f555842
ArchanaKumarasamy <archanatk03@gmail.com>
2024-07-15 20:34:50 +0530

    completed

--- JavaScript_Programming_Exercises-master/12_JS_Sets_Maps/Ch12_pgm_03.js
+++ JavaScript_Programming_Exercises-master/12_JS_Sets_Maps/Ch12_pgm_03.js
@@ -1,6 +1,48 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
+
+const url = 'https://restcountries.com/v2/all';
+
+
+const mostSpokenLanguages = async (countries, n) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    const languageCount = {};
+
+
+    data.forEach(country => {
+      country.languages.forEach(language => {
+        if (languageCount[language.name]) {
+          languageCount[language.name]++;
+        } else {
+          languageCount[language.name] = 1;
+        }
+      });
+    });
+
+
+    const languageArray = Object.keys(languageCount).map(language => {
+      return { [language]: languageCount[language] };
+    });
+
+
+    languageArray.sort((a, b) => {
+      const countA = Object.values(a)[0];
+      const countB = Object.values(b)[0];
+      return countB - countA;
+    });
+
+
+    return languageArray.slice(0, n);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+
+mostSpokenLanguages(countries, 10).then(result => console.log(result));
+mostSpokenLanguages(countries, 3).then(result => console.log(result));
+
 
 // How many languages are there in the countries object file.
 
